efc3c7ca14e2915dc389c47882a45b727f31f56b
Sergey Privalov <kalabanga12@yandex.ru>
2019-08-26 14:27:12 +0500

    Выполнен Урок 1

---
+++ lesson_01/js/main.js
@@ -0,0 +1,13 @@
+let money,
+    income,
+    addExpenses,
+    deposit,
+    mission,
+    period,
+    nameUser = "Sergey";
+
+alert("Hello Sensei!!!");
+
+console.log('Name :', nameUser);
+
+console.error();
