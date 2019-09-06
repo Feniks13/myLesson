c4f699ddeaaeb4a5418c005caa393b6934fa52d2
Sergey Privalov <kalabanga12@yandex.ru>
2019-09-06 21:09:17 +0500

    Сделана начальная разметка разметка

---
+++ task_01/js/main.js
@@ -0,0 +1,18 @@
+'use strict';
+let a = document.querySelector('#a'),        /* Поле переменной а */
+    b = document.querySelector('#b'),        /* Поле переменной b */
+    sum = document.querySelector('#sum'),    /* Кнопка сумма */
+    mult = document.querySelector('#mult'),   /* Кнопка произведение */
+    res = document.querySelector('#res');     /* Поле результата */
+
+const calculator = {
+  sum: function(){
+    // ваш код
+  },
+  mult: function(){
+    // ваш код
+  },
+  show: function(){
+    // ваш код
+  }
+};
