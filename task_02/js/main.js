9597d08da0767ce7fa8270d8e59177e469a4274a
Sergey Privalov <kalabanga12@yandex.ru>
2019-09-06 23:09:34 +0500

    Выполнено задание 2

--- task_02/js/main.js
+++ task_02/js/main.js
@@ -2,8 +2,15 @@
 
 function getResult(a,b){
   let result;
-  // ваш код
-  return result
+  result = a ** b + '';         /* Возвели в степень и преобразуем в строку */
+  result = result.split('');    /* Разбиваем массив */
+  result = result.reduce(function(previousValue, currentItem) {   /* Перебирающий метод массива reduce */
+    //console.log(previousValue);
+    //console.log(currentItem);
+    return (+previousValue) + (+currentItem);
+    
+  });
+  return result;
 }
 
 console.log(getResult(3, 10));
