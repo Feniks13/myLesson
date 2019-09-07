a85341b7d34395771e8f23e36dd18f1a682f5161
Sergey Privalov <kalabanga12@yandex.ru>
2019-09-07 16:22:07 +0500

    Внёс перменные

--- task_03/js/main.js
+++ task_03/js/main.js
@@ -1,4 +1,7 @@
 'use strict';
+let country = document.querySelector('#country'),   /* Список стран */
+    city = document.querySelector('#city'),         /* Список городов */
+    result = document.querySelector('.result');     /* Вывод результата */
 
 const cityArr = {
   rus: ['Москва', 'Санк-Петербург', 'Новосибирск', 'Екатеринбург', 'Нижний Новгород', 'Казань', 'Челябинск'],
@@ -6,3 +9,5 @@ const cityArr = {
   bel: ['Минск', 'Гомель', 'Могилёв', 'Витебск', 'Гродно', 'Брест'],
   jap: ['Токио', 'Киото', 'Осака', 'Иокогама'] 
 };
+
+console.log(country.value);
