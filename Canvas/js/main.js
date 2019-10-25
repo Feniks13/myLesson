8f1b950be1c7071c8220d1c780d902f23ae1d1f5
Sergey Privalov <serzh.privalov.86@mail.ru>
2019-10-25 16:12:39 +0500

    Выполнен урок

--- Canvas/js/main.js
+++ Canvas/js/main.js
@@ -2,7 +2,11 @@ window.addEventListener('DOMContentLoaded', () => {           // Ждём заг
   'use strict';
 
   const canvas = document.getElementById('canvas'),
-    ctx = canvas.getContext('2d');
+    canvas2 = document.getElementById('canvas2'),
+    canvas3 = document.getElementById('canvas3'),
+    ctx = canvas.getContext('2d'),
+    ctx2 = canvas2.getContext('2d'),
+    ctx3 = canvas3.getContext('2d');
     //gradient = ctx.createLinearGradient(20, 20, 120, 120), // Градиент (начальная точка(X, Y) и конечная (X, Y))
     // градиент круга
     //(Первый круг(X, Y, Радиус), Второй круг(X, Y, Радиус))
@@ -47,18 +51,89 @@ window.addEventListener('DOMContentLoaded', () => {           // Ждём заг
 
   ctx.moveTo(175, 150);       // Точка отсчёта
   //(X, Y, Радиус, угол начала, угол конца, по часовой)
-  ctx.arc(150, 150, 25, 0, angle(345), false);           // Дуга
+  ctx.arc(150, 150, 25, 0, angle(), false);           // Дуга
+
+  ctx.moveTo(125, 125);
+  ctx.arcTo(150, 100, 175, 125, 30);
+  ctx.lineTo(175,125);
 
   ctx.lineWidth = '2';         // Толщина линий
   ctx.strokeStyle = '#008844';
 
+
+  //Кривая безье
+  ctx.moveTo(100, 100);
+  ctx.bezierCurveTo(200, 0, 200, 200, 300, 100);
+
+  
   ctx.stroke();                 // Конец контура
+  
+  //Текст
+  ctx.shadowOffsetX = 5;
+  ctx.shadowOffsetY = 5;
+  ctx.shadowBlur = 3;
+  ctx.shadowColor = 'green';
+  ctx.font = '30px sans-serif';
+  ctx.fillStyle = 'red';
+  ctx.save();     //Сохраняем состояние
+  ctx.fillText('JavaScript', 50, 50, 200);
 
+  ctx.shadowOffsetX = 7;
+  ctx.shadowOffsetY = 3;
+  ctx.shadowBlur = 6;
+  ctx.shadowColor = 'black';
+  ctx.font = '30px sans-serif';
+  ctx.fillStyle = 'orange';
+  ctx.rotate(angle(10));      // Поворачиваем
+  ctx.fillText('Glo Academy', 200, 50, 200);
+  ctx.restore();              //Возвращаем сохранённое состояние
+  ctx.fillText('Freelance', 125, 250, 200);
 
+  
+  let tick = 0;  
+  const animation = () => {
+    ctx2.clearRect(0,0, canvas2.clientWidth, canvas2.clientHeight);
+    ctx2.fillStyle = 'green';
+    ctx2.fillRect(tick, tick, 50, 50);
+    tick ++;
+    if (tick < 250) {
+      requestAnimationFrame(animation);
+    } else {
+      reverse();
+    }
+  };
 
+  const reverse = () => {
+    ctx2.clearRect(0,0, canvas2.clientWidth, canvas2.clientHeight);
+    ctx2.fillStyle = 'green';
+    ctx2.fillRect(tick, tick, 50, 50);
+    tick --;
+    if (tick > 0) {
+      requestAnimationFrame(reverse);
+    } else {
+      animation();
+    }
+  };
+  animation();
 
+  const color = document.getElementById('color');
 
+  color.addEventListener('input', () => ctx3.strokeStyle = color.value);
+  canvas3.addEventListener('mousemove', (event) => {
+    const x = event.offsetX,
+      y = event.offsetY,
+      mx = event.movementX,
+      my = event.movementY;
 
+      if (event.buttons > 0) {
+        ctx3.beginPath();
+        ctx3.moveTo(x, y);
+        ctx3.lineTo(x - mx, y - my);
+        ctx3.stroke();
+        ctx3.closePath();
+      }
+
+  });
 
 
 });
